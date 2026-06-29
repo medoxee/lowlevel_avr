22cb60b4779a99a991732aca8a39a5ff03b51279
medoxee <medoxee99@gmail.com>
2026-06-29 02:35:01 +0100

    sht31_convert function implementation

--- components/sht31/sht31_driver.c
+++ components/sht31/sht31_driver.c
@@ -4,73 +4,78 @@
 #include <util/delay.h>
 #include <stdint.h>
 
-#define	TWBR		0x48
+// #define	TWBR		0x48
 #define	SHT_ADDR	0x44
 #define	WBIT		0b0
 #define	RBIT		0b1
 #define	CMD_MSB		0x24
 #define	CMD_LSB		0x0b
 #define	DATA_SIZE	6
+#define	CRC_INIT 	0xff
+#define	CRC_POLY	0x31
+#define	CRC_ERR 	0xcc
 
-uint8_t	sht31_interrupt_handler(uint8_t	status)
+
+uint8_t	sht31_interrupt_handler(uint8_t	status_code)
 {
 	TWCR |= (1 << TWINT) | (1 << TWEN);
 	while (!(TWCR & (1 << TWINT)));
-	if (TW_STATUS != status)
+	if (TW_STATUS != status_code)
 		return 1;
 	return 0;
 }
 
 void	sht31_send_condition(uint8_t	condition)
 {	
-	TWCR |= (1 << condition) | (1 << TWINT) | (1 << TWEN);
+	TWCR = (1 << condition) | (1 << TWINT) | (1 << TWEN);
 	if (condition != TWSTO)
 		while (!(TWCR & (1 << TWINT)));
 }
 
-int8_t	sht31_init(void)
+uint8_t	sht31_init(void)
 {
-	/* set prescaller to 0b00.
+	/* 
+	 * set bit rate to 0x48 (calculated from formula of sensor manifacturer)
+	 * set prescaller to 0b00.
 	 * set & send START condition.
 	 * wait until it is sent.
 	 * reset TWSTA and check if START is transmitted.
 	 * if not, flag an error.
 	 * create i2c addr + W bit then send it, then check ACK.
 	 */
+	TWBR = 0x48;
 	TWSR &= 0xfc;
 	sht31_send_condition(TWSTA);
 	if (sht31_interrupt_handler(TW_START))
-		return -1;
+		return 0x01;
 	TWCR &= ~(1 << TWSTA);
 	TWDR = (SHT_ADDR << 1) | WBIT;
 	if (sht31_interrupt_handler(TW_MT_SLA_ACK))
-		return -2;
+		return 0x02;
 	TWDR = CMD_MSB;
 	if (sht31_interrupt_handler(TW_MT_DATA_ACK))
-		return -3;
+		return 0x03;
 	TWDR = CMD_LSB;
 	if (sht31_interrupt_handler(TW_MT_DATA_ACK))
-		return -4;
+		return 0x04;
 	sht31_send_condition(TWSTO);
-	if (sht31_interrupt_handler(TW_STOP))
-		return -5;
 	TWCR &= ~(1 << TWSTO);
 	_delay_ms(8);
 	return 0;
 }
 
-int8_t	sht31_read_data(void)
+uint8_t	sht31_read_data(void)
 {
 	uint8_t	data[DATA_SIZE];
 	uint8_t	byte_counter;
 
 	sht31_send_condition(TWSTA);
 	if (sht31_interrupt_handler(TW_START))
-		return	-6;
+		return	0x06;
 	TWCR &= ~(1 << TWSTA);
 	TWDR = (SHT_ADDR << 1) | RBIT;
 	if (sht31_interrupt_handler(TW_MR_SLA_ACK))
-		return	-7;
+		return	0x07;
 	byte_counter = 0;
 	TWCR |= (1 << TWEA);
 	while  (1)
@@ -79,21 +84,21 @@ int8_t	sht31_read_data(void)
 		{
 			TWCR &= ~(1 << TWEA);
 			if (sht31_interrupt_handler(TW_MR_DATA_NACK))
-				return	-8;
+				return	0x08;
 			data[byte_counter] = TWDR;
 			break;
 		}
 		if (sht31_interrupt_handler(TW_MR_DATA_ACK))
-			return	-9;
+			return	0x09;
 		data[byte_counter] = TWDR;
 		byte_counter++;
 	}
 	sht_send_condition(TWSTO);
 	TWCR &= ~(1 << TWSTO);
 	if (sht31_verif_crc(data[0], data[1]) != data[2])
-		return	-11;
+		return	0x0A;
 	if (sht31_verif_crc(data[3], data[4]) != data[5])
-		return	-12;
+		return	0x0B;
 	return	0;
 }
 
@@ -122,4 +127,18 @@ uint8_t	sht31_verif_crc(uint8_t	data_msb, uint8_t	data_lsb)
 		xsb_byte++;
 	}
 	return	acc;
+}
+
+uint16_t	*sht31_convert(uint16_t	temp_msb, uint16_t	temp_lsb,
+		uint16_t	hum_msb, uint16_t	hum_lsb)
+{
+	uint16_t	raw_temp;
+	uint16_t	raw_hum;
+	uint16_t	scaled_data[2];
+
+	raw_temp = (temp_msb << 8) | temp_lsb;
+	raw_hum = (hum_msb << 8) | hum_lsb;
+	scaled_data[0] = -45 + 175 * (raw_temp / 65535);
+	scaled_data[1] = 100 * (raw_hum / 65535);
+	return	scaled_data;
 }
